1f73ff1650074de33286dce365e546d708885f04
Juillard Mathis <mathis.juillard@edu.univ-fcomte.fr>
2025-06-01 14:49:15 +0200

    feat: add @astrojs/alpinejs dependency to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import netlify from '@astrojs/netlify';
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
