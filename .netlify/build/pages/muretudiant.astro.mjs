08b6a30728a362b8b0f9aac5a853b702f40baed5
Juillard Mathis <mathis.juillard@edu.univ-fcomte.fr>
2025-06-02 09:40:24 +0200

    feat: Add new pages and features for Fac'Link platform
    - Created a new "Connecte" page with a welcome message.
    - Implemented a "Contact" page with a form for user inquiries.
    - Added "Creation Evenement" page for event creation (currently empty).
    - Developed an "Evenements" page to list events fetched from the backend.
    - Introduced a "Fonctionnalite" page detailing the features of Fac'Link.
    - Built the main "Index" page with an introduction and various sections about the platform.
    - Established an "Inscription" page for user registration with form validation and social media links.
    - Updated "Muretudiant" pages to handle dynamic student profiles.
    - Configured SSR settings for the application.

---
+++ .netlify/build/pages/muretudiant.astro.mjs
@@ -0,0 +1,24 @@
+import { f as createComponent, m as maybeRenderHead, r as renderTemplate } from '../chunks/astro/server_Dc7gvlnh.mjs';
+import 'kleur/colors';
+import 'clsx';
+export { renderers } from '../renderers.mjs';
+
+const prerender = false;
+const $$Index = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`${maybeRenderHead()}<a href="../evenements/">page evenements</a>`;
+}, "/Users/admin/Documents/GitHub/projet-co-s2-2025-08_faclink/src/pages/muretudiant/index.astro", void 0);
+
+const $$file = "/Users/admin/Documents/GitHub/projet-co-s2-2025-08_faclink/src/pages/muretudiant/index.astro";
+const $$url = "/muretudiant";
+
+const _page = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+	__proto__: null,
+	default: $$Index,
+	file: $$file,
+	prerender,
+	url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+const page = () => _page;
+
+export { page };
