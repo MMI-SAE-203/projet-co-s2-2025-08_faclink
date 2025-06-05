472c6c1e1f2d0a2cf5564c3cd668f3b720ab1905
Juillard Mathis <mathis.juillard@edu.univ-fcomte.fr>
2025-06-05 15:40:28 +0200

    feat: Add PocketBase dependency to project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import alpinejs from '@astrojs/alpinejs';
-
 import netlify from '@astrojs/netlify';
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   output: 'server',
   integrations: [alpinejs(), sitemap()],
   adapter: netlify(),
   site: "https://faclink.netlify.app",
 
+  // 💡 Pour éviter que PocketBase casse le build
+  build: {
+    rollupOptions: {
+      external: ['pocketbase']
+    }
+  }
 });
